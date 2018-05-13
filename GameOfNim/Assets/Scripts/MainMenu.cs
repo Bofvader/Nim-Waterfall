1a7080d02a2fffaeb7e97d4d6c5337f4dc7cb12b
bdorman <bdorman@student.neumont.edu>
2018-05-12 19:25:16 -0600

    MainMenu is filled out for now

--- GameOfNim/Assets/Scripts/MainMenu.cs
+++ GameOfNim/Assets/Scripts/MainMenu.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class MainMenu : MonoBehaviour
+{
+	public void GoToQuit()
+	{
+		SceneManager.LoadScene(1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void GoToRules()
+	{
+		SceneManager.LoadScene("RulesMenu");
+	}
+
+	public void GoToSetup()
+	{
+		SceneManager.LoadScene("SetupMenu");
 	}
 }
